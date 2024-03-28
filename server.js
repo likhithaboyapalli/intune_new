92525937c15b14d0c42b677d05b99671a4ad55b7
Likhitha Reddy <119039306+BoyapalliLikhitha@users.noreply.github.com>
2024-03-28 14:44:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ async function authforApp(clientId, clientSecret, tenantId) {
     }
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8668;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
